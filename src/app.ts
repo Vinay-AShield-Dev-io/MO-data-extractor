81c4d5e4a6f7373b4ff4d1d20786e44684422698
Vinay-AShield-Dev-io <96040758+Vinay-AShield-Dev-io@users.noreply.github.com>
2024-02-19 15:24:51 +0530

    Dev (#6)
    * removed hardcodes
    
    * prettyfied outputs and more insights
    
    * Update README.md
    
    * functionalizing code
    
    * modify email addresses
    
    * modify email addresses
    
    * changing header from initiated to failed
    
    * NaN bug fix
    
    * typescript packages included
    
    * adding schedular functions
    
    * added docker files
    
    * Docker file changes
    
    * added sms timelapse generating script

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 
+import { Db, MongoClient } from 'mongodb';
 import { performance } from 'perf_hooks';
 import { longCodesInfo } from '../config/config';
 import { sendMail } from './azureEmailService';
@@ -17,32 +18,30 @@ if (process.argv.length > 3) {
 
 
 // 'This function runs at every hour\'s 5th minute'
-const execScript = () => {
-    getDB().then(async (resp) => {
-        if (resp === null) return;
-        const [db, client] = resp;
-        const ashieldmobtxnCollection = db.collection("ashieldmobtxn");
-        console.log("Time taken to connect to the MongoDB:", (performance.now() - performaStartTime).toFixed(2), "ms");
-
-        let finalFormattedOUtput = ""
-        for (const item of longCodesInfo) {
-            finalFormattedOUtput += `<h4> ${item.name} (${item.code}) </h4>`;
-            const [data, completed = 0, expired = 0, failed = 0] = await getLongCodeDataSummary(ashieldmobtxnCollection, item.code, startDate, endDate);
-            finalFormattedOUtput += data;
-            const totalRequests: number = completed + failed + expired;
-            finalFormattedOUtput += "Total requests = " + totalRequests + "<br>";
-            const PercentageOfExpiredRate = expired === 0 ? 0 : ((expired / totalRequests) * 100).toFixed(2)
-            const PercentageOfFailedRate = failed === 0 ? 0 : ((failed / totalRequests) * 100).toFixed(2)
-            finalFormattedOUtput += `Expired rate in % = ${PercentageOfExpiredRate} <br>`;
-            finalFormattedOUtput += `Failed rate in % = ${PercentageOfFailedRate}<br>`;
-        };
-        await client.close();
-        console.log("Time taken to execute mongo queries:", (performance.now() - performaStartTime).toFixed(2), "ms");
-        await sendMail(finalFormattedOUtput);
-        console.log("Program execution completed:", (performance.now() - performaStartTime).toFixed(2), "ms");
-    }).catch(err => {
-        console.log(err);
-    })
+const execScript = async () => {
+    let client: MongoClient
+    let db: Db
+    const resp = getDB();
+    if (resp === null) return;
+    [db, client] = resp;
+    const ashieldmobtxnCollection = db.collection("ashieldmobtxn");
+    console.log("Time taken to connect to the MongoDB:", (performance.now() - performaStartTime).toFixed(2), "ms")
+    let finalFormattedOUtput = ""
+    for (const item of longCodesInfo) {
+        finalFormattedOUtput += `<h4> ${item.name} (${item.code}) </h4>`;
+        const [data, completed = 0, expired = 0, failed = 0] = await getLongCodeDataSummary(ashieldmobtxnCollection, item.code, startDate, endDate);
+        finalFormattedOUtput += data;
+        const totalRequests: number = completed + failed + expired;
+        finalFormattedOUtput += "Total requests = " + totalRequests + "<br>";
+        const PercentageOfExpiredRate = expired === 0 ? 0 : ((expired / totalRequests) * 100).toFixed(2)
+        const PercentageOfFailedRate = failed === 0 ? 0 : ((failed / totalRequests) * 100).toFixed(2)
+        finalFormattedOUtput += `Expired rate in % = ${PercentageOfExpiredRate} <br>`;
+        finalFormattedOUtput += `Failed rate in % = ${PercentageOfFailedRate}<br>`;
+    };
+    client.close();
+    console.log("Time taken to execute mongo queries:", (performance.now() - performaStartTime).toFixed(2), "ms");
+    sendMail(finalFormattedOUtput);
+    console.log("Program execution completed:", (performance.now() - performaStartTime).toFixed(2), "ms");
 }
 
 
--- src/connectDB.ts
+++ src/connectDB.ts
@@ -3,7 +3,8 @@ import { ENV_VALS } from '../config/config';
 const MONGO_URL = ENV_VALS.mongoURL;
 const dbName = ENV_VALS.mongoDB_DBName;
 
-export const getDB = async (): Promise<[Db, MongoClient] | null> => {
+
+export const getDB = (): [Db, MongoClient] | null => {
     console.log(MONGO_URL);
     if (MONGO_URL === "") { console.error("MONGO_URL is not set"); return null };
     const client = new MongoClient(MONGO_URL);
